0818a2247e1325a5f24c97a63924a1262bc41fdc
Nick Selivanov <edu.selivanov@yandex.ru>
2025-05-16 07:27:35 +0300

    restauran list layout

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from "react-dom/client";
+import { restaurants } from "./materials/mock";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+  <div>
+    {restaurants.map((restauran) => (
+      <div key={restauran.id}>
+        <h1>{restauran.name}</h1>
+        <h3>Меню</h3>
+        <ul>
+          {restauran.menu.map((menuItem) => (
+            <li key={menuItem.id}>{menuItem.name}</li>
+          ))}
+        </ul>
+        <h3>Отзывы</h3>
+        <ul>
+          {restauran.reviews.map((reviewItem) => (
+            <li key={reviewItem.id}>{reviewItem.text}</li>
+          ))}
+        </ul>
+      </div>
+    ))}
+  </div>
+);
--- src/materials/mock.ts
+++ src/materials/mock.ts
@@ -1,22 +1,29 @@
-export const restaurants = [
+import type {
+  MenuId,
+  Restauran,
+  RestaurantId,
+  ReviewId,
+} from "../types/app.types";
+
+export const restaurants: Restauran[] = [
   {
-    id: "a757a0e9-03c1-4a2a-b384-8ac21dbe2fb2",
+    id: "a757a0e9-03c1-4a2a-b384-8ac21dbe2fb2" as RestaurantId,
     name: "Dishoom",
     menu: [
       {
-        id: "d75f762a-eadd-49be-8918-ed0daa8dd024",
+        id: "d75f762a-eadd-49be-8918-ed0daa8dd024" as MenuId,
         name: "Chicken tikka masala",
         price: 12,
         ingredients: ["chicken", "rice"],
       },
       {
-        id: "c3cb8f92-a2ed-4716-92a1-b6ea813e9049",
+        id: "c3cb8f92-a2ed-4716-92a1-b6ea813e9049" as MenuId,
         name: "Naan",
         price: 3,
         ingredients: ["bread"],
       },
       {
-        id: "bd129641-c0eb-432b-84b6-8b81d2930358",
+        id: "bd129641-c0eb-432b-84b6-8b81d2930358" as MenuId,
         name: "Samosa",
         price: 8,
         ingredients: ["chicken", "bread"],
@@ -24,13 +31,13 @@ export const restaurants = [
     ],
     reviews: [
       {
-        id: "5909796d-5030-4e36-adec-68b8f9ec2d96",
+        id: "5909796d-5030-4e36-adec-68b8f9ec2d96" as ReviewId,
         user: "Antony",
         text: "Not bad",
         rating: 5,
       },
       {
-        id: "429dea85-11dd-4054-a31e-c60c92e17255",
+        id: "429dea85-11dd-4054-a31e-c60c92e17255" as ReviewId,
         user: "Sam",
         text: "No burgers",
         rating: 3,
@@ -38,17 +45,17 @@ export const restaurants = [
     ],
   },
   {
-    id: "bb8afbec-2fec-491f-93e9-7f13950dd80b",
+    id: "bb8afbec-2fec-491f-93e9-7f13950dd80b" as RestaurantId,
     name: "Homeslice",
     menu: [
       {
-        id: "25402233-0095-49ea-9939-1e67ed89ffb9",
+        id: "25402233-0095-49ea-9939-1e67ed89ffb9" as MenuId,
         name: "Margarita",
         price: 9,
         ingredients: ["bread", "cheese", "tomatoes"],
       },
       {
-        id: "90902233-0095-49ea-9939-1e67ed89ffb9",
+        id: "90902233-0095-49ea-9939-1e67ed89ffb9" as MenuId,
         name: "Chef pizza",
         price: 10,
         ingredients: ["bread", "cheese", "tomatoes", "chicken"],
@@ -56,19 +63,19 @@ export const restaurants = [
     ],
     reviews: [
       {
-        id: "53b642d7-5e86-4717-a466-0640a1dee076",
+        id: "53b642d7-5e86-4717-a466-0640a1dee076" as ReviewId,
         user: "Diana",
         text: "Perfect Margarita",
         rating: 5,
       },
       {
-        id: "c27ab88e-375c-4e98-aa94-8a180150a797",
+        id: "c27ab88e-375c-4e98-aa94-8a180150a797" as ReviewId,
         user: "Sam",
         text: "No burgers again. But Chef Pizza is the best one",
         rating: 4,
       },
       {
-        id: "abc0c5e1-cd57-4f0a-99d9-00e6b4533b3a",
+        id: "abc0c5e1-cd57-4f0a-99d9-00e6b4533b3a" as ReviewId,
         user: "Lolly",
         text: "Good for lunch",
         rating: 5,
@@ -76,23 +83,23 @@ export const restaurants = [
     ],
   },
   {
-    id: "982bfbce-c5e0-41a0-9f99-d5c20ecee49d",
+    id: "982bfbce-c5e0-41a0-9f99-d5c20ecee49d" as RestaurantId,
     name: "Fabrique",
     menu: [
       {
-        id: "08c9ffa0-d003-4310-9e15-20978743296e",
+        id: "08c9ffa0-d003-4310-9e15-20978743296e" as MenuId,
         name: "Cinnamon buns",
         price: 5,
         ingredients: ["bread"],
       },
       {
-        id: "64a4967c-2080-4a99-9074-4655a4569a95",
+        id: "64a4967c-2080-4a99-9074-4655a4569a95" as MenuId,
         name: "Semlor",
         price: 2,
         ingredients: ["bread", "cream"],
       },
       {
-        id: "4bc8528e-26d1-46c3-a522-8e18d10c8c84",
+        id: "4bc8528e-26d1-46c3-a522-8e18d10c8c84" as MenuId,
         name: "Saffron bun",
         price: 4,
         ingredients: ["bread"],
@@ -100,7 +107,7 @@ export const restaurants = [
     ],
     reviews: [
       {
-        id: "53b642d7-5e86-4717-a466-0640a1dee076",
+        id: "53b642d7-5e86-4717-a466-0640a1dee076" as ReviewId,
         user: "Agata",
         text: "Best bakery",
         rating: 5,
@@ -108,17 +115,17 @@ export const restaurants = [
     ],
   },
   {
-    id: "d9241927-09e1-44f3-8986-a76346869037",
+    id: "d9241927-09e1-44f3-8986-a76346869037" as RestaurantId,
     name: "Flat Iron",
     menu: [
       {
-        id: "6c02c2ce-b868-4191-b4a7-8686429f4bac",
+        id: "6c02c2ce-b868-4191-b4a7-8686429f4bac" as MenuId,
         name: "Flat Iron Steak",
         price: 10,
         ingredients: ["beef"],
       },
       {
-        id: "99bb6fbb-e53b-4b7e-b9c2-23b63b77385d",
+        id: "99bb6fbb-e53b-4b7e-b9c2-23b63b77385d" as MenuId,
         name: "Flat Iron Burger",
         price: 10,
         ingredients: ["bread", "beef"],
@@ -126,13 +133,13 @@ export const restaurants = [
     ],
     reviews: [
       {
-        id: "5db6247b-ab1c-49db-be1f-8dd27fd38b81",
+        id: "5db6247b-ab1c-49db-be1f-8dd27fd38b81" as ReviewId,
         user: "Sam",
         text: "Finally! This place is amazing place for breakfast, lunch, dinner and supper",
         rating: 5,
       },
       {
-        id: "381b0c31-6360-43ff-80d1-581a116159d8",
+        id: "381b0c31-6360-43ff-80d1-581a116159d8" as ReviewId,
         user: "Rebeca",
         text: "Meat here is extremely delicious",
         rating: 5,
