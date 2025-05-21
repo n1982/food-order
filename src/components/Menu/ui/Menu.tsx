1a9f221059f6a2cfb87ce9376ea5357db0b61779
Nick Selivanov <edu.selivanov@yandex.ru>
2025-05-21 17:27:00 +0300

    menu styles

--- src/components/Menu/ui/Menu.tsx
+++ src/components/Menu/ui/Menu.tsx
@@ -2,19 +2,21 @@ import type { FC } from "react";
 import type { IDish } from "../../../types/app.types";
 import Dish from "../../Dish";
 
+import "./Menu.css";
+
 interface IMenuProps {
   menu: IDish[];
 }
 const Menu: FC<IMenuProps> = ({ menu }) => {
   return (
-    <>
-      <h3>Меню</h3>
-      <ul>
+    <section className='menu-container'>
+      <h3 className='menu-title'>Меню</h3>
+      <ul className='menu-list'>
         {menu.map(({ id, name }) => (
           <Dish key={id} name={name} />
         ))}
       </ul>
-    </>
+    </section>
   );
 };
 export default Menu;
