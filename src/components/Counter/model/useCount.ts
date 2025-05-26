41ea9b1a6a628e1cfab2e9cf603a97d6dbcc0151
Nick Selivanov <edu.selivanov@yandex.ru>
2025-05-26 06:18:49 +0300

    min/max value in useCount

--- src/components/Counter/model/useCount.ts
+++ src/components/Counter/model/useCount.ts
@@ -3,7 +3,7 @@ import { useState } from "react";
 export const useCount = () => {
   const [count, setCount] = useState(0);
 
-  const onIncrement = () => setCount((prev) => prev + 1);
-  const onDecrement = () => setCount((prev) => prev - 1);
+  const onIncrement = () => setCount((prev) => Math.min(prev + 1, 5));
+  const onDecrement = () => setCount((prev) => Math.max(prev - 1, 0));
   return { count, onIncrement, onDecrement };
 };
--- src/components/Counter/ui/Counter.tsx
+++ src/components/Counter/ui/Counter.tsx
@@ -2,20 +2,13 @@ import { useCount } from "../model/useCount";
 import "./Counter.css";
 const Counter = () => {
   const { count, onIncrement, onDecrement } = useCount();
-  const handleDecrementClick = () => {
-    if (count > 0) onDecrement();
-  };
-
-  const handleIncrementClick = () => {
-    if (count < 5) onIncrement();
-  };
   return (
     <div className='counter'>
-      <button className='counter-button' onClick={handleDecrementClick}>
+      <button className='counter-button' onClick={onDecrement}>
         -
       </button>
       <span className='counter-value'>{count}</span>
-      <button className='counter-button' onClick={handleIncrementClick}>
+      <button className='counter-button' onClick={onIncrement}>
         +
       </button>
     </div>
