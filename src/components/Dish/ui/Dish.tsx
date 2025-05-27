7d58f101694c58c812fc0f7d059d48caeff802ad
Nick Selivanov <edu.selivanov@yandex.ru>
2025-05-27 22:16:07 +0300

    refactoring dish component

--- src/components/Dish/ui/Dish.tsx
+++ src/components/Dish/ui/Dish.tsx
@@ -1,5 +1,6 @@
 import type { FC } from "react";
 import Counter from "../../Counter";
+import { useCount } from "../../Counter/model/useCount";
 
 import "./Dish.css";
 
@@ -7,12 +8,17 @@ interface DishProps {
   name: string;
 }
 const Dish: FC<DishProps> = ({ name }) => {
+  const { count, onDecrement, onIncrement } = useCount();
   return (
     <>
       <li className='dish-item'>
         <span className='dish-name'>{name}</span>
         <div className='dish-controls'>
-          <Counter />
+          <Counter
+            count={count}
+            onDecrement={onDecrement}
+            onIncrement={onIncrement}
+          />
         </div>
       </li>
     </>
