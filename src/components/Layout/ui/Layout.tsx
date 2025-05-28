abc8fde008e649f65f242091a76a71e8f4f11596
Nick Selivanov <edu.selivanov@yandex.ru>
2025-05-28 16:32:13 +0300

    layout styles

--- src/components/Layout/ui/Layout.tsx
+++ src/components/Layout/ui/Layout.tsx
@@ -1,6 +1,6 @@
 import type { ReactNode } from "react";
 
-import "./Layout.css";
+import styles from "./Layout.module.css";
 
 interface LayoutProps {
   children: ReactNode;
@@ -8,10 +8,10 @@ interface LayoutProps {
 
 const Layout = ({ children }: LayoutProps) => {
   return (
-    <div className='layout'>
-      <header className='layout-header'>Header</header>
-      <main className='layout-content'>{children}</main>
-      <footer className='layout-footer'>footer</footer>
+    <div className={styles.layout}>
+      <header className={styles.layout_header}>Header</header>
+      <main className={styles.layout_content}>{children}</main>
+      <footer className={styles.layout_footer}>footer</footer>
     </div>
   );
 };
