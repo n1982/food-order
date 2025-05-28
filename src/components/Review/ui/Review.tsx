590a48042895307d3826d257f391e2142accb342
Nick Selivanov <edu.selivanov@yandex.ru>
2025-05-28 16:16:18 +0300

    review styles

--- src/components/Review/ui/Review.tsx
+++ src/components/Review/ui/Review.tsx
@@ -1,10 +1,12 @@
 import type { FC } from "react";
 
+import styles from "./Review.module.css";
+
 interface ReviewProps {
   text: string;
 }
 const Review: FC<ReviewProps> = ({ text }) => {
-  return <li>{text}</li>;
+  return <li className={styles.review}>{text}</li>;
 };
 
 export default Review;
