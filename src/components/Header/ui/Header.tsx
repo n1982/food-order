b6355c84d641aa6744ba3b2a101ac5b3b3650b51
Nick Selivanov <edu.selivanov@yandex.ru>
2025-06-01 20:15:50 +0300

    кнопка переключения темы

--- src/components/Header/ui/Header.tsx
+++ src/components/Header/ui/Header.tsx
@@ -6,7 +6,11 @@ export const Header = forwardRef<HTMLDivElement>((_, ref) => {
     return (
         <header ref={ref} className={styles.header}>
             <span>Header</span>
+            <div className={styles.button_wrapper}>
+            <button className={styles.button}>Theme</button>
             <button className={styles.button}>Login</button>
+
+            </div>
         </header>
     );
 });
