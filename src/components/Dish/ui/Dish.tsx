5c350d9c11e7e017413cb9569a6bc9caf686a318
Nick Selivanov <edu.selivanov@yandex.ru>
2025-06-01 12:06:30 +0300

    fix class name Dish component

--- src/components/Dish/ui/Dish.tsx
+++ src/components/Dish/ui/Dish.tsx
@@ -2,7 +2,7 @@ import type { FC } from "react";
 import { Counter } from "../../Counter";
 import { useCount } from "../../Counter/model/useCount";
 
-import "./Dish.css";
+import styles from "./Dish.module.css";
 
 interface DishProps {
   name: string;
@@ -11,9 +11,9 @@ export const Dish: FC<DishProps> = ({ name }) => {
   const { count, onDecrement, onIncrement } = useCount();
   return (
     <>
-      <li className='dish-item'>
-        <span className='dish-name'>{name}</span>
-        <div className='dish-controls'>
+      <li className={styles.wrapper}>
+        <span className={styles.name}>{name}</span>
+        <div className={styles.quantity}>
           <Counter
             count={count}
             onDecrement={onDecrement}
@@ -25,4 +25,4 @@ export const Dish: FC<DishProps> = ({ name }) => {
   );
 };
 
-export default Dish;
+
