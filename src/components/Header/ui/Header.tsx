bd79889498426f8c6452b20a39785bb038e180a4
Nick Selivanov <edu.selivanov@yandex.ru>
2025-06-02 18:03:13 +0300

    переключение темы

--- src/components/Header/ui/Header.tsx
+++ src/components/Header/ui/Header.tsx
@@ -1,14 +1,17 @@
-import {forwardRef} from "react";
+import {forwardRef, useContext} from "react";
 import styles from "./Header.module.css";
 import {Button} from "../../Button";
+import {ThemeContext} from "../../App/model/ThemeContext.ts";
 
 
 export const Header = forwardRef<HTMLDivElement>((_, ref) => {
+    const {toggleTheme} = useContext(ThemeContext)
+
     return (
         <header ref={ref} className={styles.header}>
             <span>Header</span>
             <div className={styles.button_wrapper}>
-                <Button className={styles.button} title={'Theme'}/>
+                <Button className={styles.button} title={'Theme'} onClick={toggleTheme}/>
                 <Button className={styles.button} title={'Login'}/>
             </div>
         </header>
